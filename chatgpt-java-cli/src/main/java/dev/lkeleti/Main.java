25ba9b02172ed63eb2416ff1bdcb7142b119f014
lkeleti <keleti.kaszlo@vipmail.hu>
2023-02-09 07:59:43 +0100

    Refactored

--- chatgpt-java-cli/src/main/java/dev/lkeleti/Main.java
+++ chatgpt-java-cli/src/main/java/dev/lkeleti/Main.java
@@ -1,14 +1,6 @@
 package dev.lkeleti;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
